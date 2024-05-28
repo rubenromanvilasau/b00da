2c4961a8137dcb60c88431859e052ee1b7218229
RubenRoman <ruben.roman@forcast.cl>
2024-05-28 18:31:15 -0400

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+const startDateThisYear = 1709305200000; // 01/03/2024 12:00pm
+const startDateLastYear = 1677682800000; // 01/03/2023 12:00pm
+
+const timestampThisYear = 1709308800000; // 01/03/2024 1:00pm
+const timestampLastYear = 1677686400000; // 01/03/2023 1:00pm
+
+const urlThisYear = `https://www.buda.com/api/v2/markets/BTC-CLP/trades?timestamp=${timestampThisYear}&limit=100`;
+const urlLastYear = `https://www.buda.com/api/v2/markets/BTC-CLP/trades?timestamp=${timestampLastYear}&limit=100`;
+
+const fetchLastYearTrades = () => {
+    return fetch(urlLastYear)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error(error));
+}
+
+const fetchThisYearTrades = () => {
+    return fetch(urlThisYear)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error(error));
+}
+
+const convertToClp = (num) => {
+    return num.toLocaleString('es-CL', { style: 'currency', currency: 'CLP'});
+}
+
+
+//BTC-CLP
+// BTC se compra - CLP se paga
+async function main(){
+
+    const [thisYearTrades, lastYearTrades] = await Promise.all([fetchThisYearTrades(), fetchLastYearTrades()])
+                                                .catch( err => {
+                                                    console.error('Error fetching trades',err);
+                                                    return;
+                                                });
+    // console.log('response', response);
+
+    console.log('-'.repeat(50));
+    //** QUESTION NUMBER 1 */
+    const filteredTradesThisYear = thisYearTrades.trades.entries.filter( entry => Number(entry[0]) >= startDateThisYear); //Filter entries where dates are greater than 01/03/2024 12:00pm
+    // console.log('trades', filteredTradesThisYear);
+
+    
+    const totalCLP = filteredTradesThisYear.reduce((acc, entry) => acc + Number(entry[2]),0);
+    console.log('Total CLP', totalCLP);
+    console.log('Formatted total in CLP', convertToClp(totalCLP));
+    /**
+     * Total would be 5.948.902.534,61
+     */
+
+    console.log('-'.repeat(50));
+    //** QUESTION NUMBER 2 */
+    
+    const lastYearFilteredTrades = lastYearTrades.trades.entries.filter( entry => Number(entry[0]) >= startDateLastYear); //Filter entries where dates are greater than 01/03/2023 12:00pm
+    // console.log('lastYearFilteredTrades', lastYearFilteredTrades);
+
+
+    const totalBTCLastYear = lastYearFilteredTrades.reduce((acc, entry) => acc + Number(entry[1]),0);
+    console.log('Last Year BTC', totalBTCLastYear);
+
+    const totalBTCThisYear = filteredTradesThisYear.reduce((acc, entry) => acc + Number(entry[1]),0);
+    console.log('This year BTC', totalBTCThisYear);
+    /**
+     * Total BTC Transacted LAST year is 0.47857174
+     * Total BTC Transacted THIS year is 0.7367471900000002
+     */
+
+    /**
+     * 100% -> 0.47857174
+     * x -> 0.7367471900000002
+     */
+
+    const percentage = (totalBTCThisYear * 100) / totalBTCLastYear;
+    console.log('Percentage increased BTC', percentage);
+    /**
+    * The percentage increased by 53.94%
+    */
+
+    console.log('-'.repeat(50));
+    //** QUESTION NUMBER 3 */
+    const budaCommission = 0.08;
+    const lostCommisions = thisYearTrades.trades.entries.reduce((acc, entry) => acc + Number(entry[2]) * budaCommission, 0);
+    console.log('Lost commisions', lostCommisions, convertToClp(lostCommisions));
+    /**
+     * Lost commissions are 475.912.202,76
+     */
+
+    console.log('-'.repeat(50));
+
+}
+
+main(); 
